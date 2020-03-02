a90ba955c7a4072cee6188e90b3fd73674502d7a
Imanuel Nusantara Jodi <imanuelnjodi@gmail.com>
2020-03-02 20:13:49 +0700

    Updates

---
+++ precache-manifest.5299343660e3a0f83f96a1b2c28ece26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24bd3e5068a82a33d739211df0dea58e",
+    "url": "/furniture-app/index.html"
+  },
+  {
+    "revision": "68cd3cfaf20729ab05d5",
+    "url": "/furniture-app/static/css/main.55be571d.chunk.css"
+  },
+  {
+    "revision": "408935fa5fab8be72857",
+    "url": "/furniture-app/static/js/2.1565f9c2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/furniture-app/static/js/2.1565f9c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68cd3cfaf20729ab05d5",
+    "url": "/furniture-app/static/js/main.4dcb31ac.chunk.js"
+  },
+  {
+    "revision": "5f41726e8925887ef85b",
+    "url": "/furniture-app/static/js/runtime-main.28ceefe4.js"
+  }
+]);
